d35e920465ee9e7f780b3138379a822bba62022e
Joshua Burger <joshberger6969@outlook.com>
2021-06-26 22:57:39 -0400

    Import Middleware for thunk, import Provider from redux so all components have access to the store, Import PostsReducer, Import redux-thunk to make fetch requests to backend API, Wrap our APP component inside of the provider and pass in the Store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { applyMiddleware, createStore, compose } from 'redux';
+import { Provider } from 'react-redux';
+import PostsReducer from './reducers/PostsReducer';
+import thunk from 'redux-thunk';
 
+const store = createStore(PostsReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
