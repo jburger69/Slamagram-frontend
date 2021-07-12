f52b476b0a3474e2779f864b3b0968d3e431d735
Joshua Burger <joshberger6969@outlook.com>
2021-07-12 15:25:01 -0400

    import picture from images and display in jsx {}

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
   
 import React from 'react';
+import Pic from '../images/pexels-luis-quintero-1787220.jpg';
 
 function Home() {
     
+    const image = <img width="500px" height="500px" className="img-responsive" src={Pic}  alt="logo"  />
     const title = <h1> Hello, Welcome to Slamagram!</h1>
     const text =   <p>This is Slamagram, a instagram clone that I made for my final project. This site has no authentication so please be careful using personal information! While using
       the app you can manage your way around using the navbar at the top. If you would like to create a new post go to the post/new path and fill out the form. When filling out the form
@@ -14,6 +16,7 @@ function Home() {
         <div>
           {title}
           {text}
+          {image}
         </div>
     )
 }
